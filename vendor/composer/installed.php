5db4240a008da3fdb0e9289b8c9c1adb96754f2c
Lulu-Soso <lucas.sokha@gmail.com>
2021-04-11 21:36:12 +0200

    add files of 1st quest Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5d5d5a9efccd2e76a24662e8350c110dd25a47aa',
+    'name' => 'lucas/composer-autoload',
+  ),
+  'versions' => 
+  array (
+    'lucas/composer-autoload' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5d5d5a9efccd2e76a24662e8350c110dd25a47aa',
+    ),
+  ),
+);
